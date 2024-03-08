8f5b686102abafafd5bc460bdea1c69294bd3b79
camiloBarranco <81373142+camiloBarranco@users.noreply.github.com>
2024-03-08 17:59:22 -0500

    Create Project

---
+++ FirstClass/app/src/main/java/com/example/firstclass/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.firstclass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edt1,edt2;
+    Button btnCalcular;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edt1 = findViewById(R.id.edt1);
+        edt2 = findViewById(R.id.edt2);
+        btnCalcular = findViewById(R.id.btnCalcular);
+    }
+}
